9491ea11ab4c474568dc0c871ff1955c029d6d88
1tpp <panapat.dev@gmail.com>
2023-03-15 14:53:38 +0700

    feat: initial project

---
+++ README.md
@@ -0,0 +1,35 @@
+# Rally
+
+## Requirements
+- python 3.10.x
+
+## Development
+- Install dependencies
+- Create a virtual environment
+- Activate the virtual environment
+- Run the application
+
+### Setup virtual environment
+```bash
+python -m venv venv
+```
+
+### Activate virtual environment
+#### mac / linux os
+```bash
+source venv/bin/activate
+```
+#### windows os
+```bash
+venv\Scripts\activate
+```
+
+### Install dependencies
+```bash
+pip install -r requirements.txt
+```
+
+### Run the server
+```bash
+uvicorn app.main:app --host 0.0.0.0 --port 3000 --reload
+```
---
+++ app/main.py
@@ -0,0 +1,92 @@
+from enum import Enum
+
+class Account:
+    def __init__(self, id, email, password, login_status):
+        self.__user_id = id
+        self.__email = email
+        self.__password = password
+        self.__login_status = login_status
+
+class User(Account):
+    def __init__(self, id, email, password, login_status, username, address, bio, profile_image):
+        super().__init__(id, email, password, login_status)
+        self.__username = username
+        self.__address = address
+        self.__bio = bio
+        self.__address = address
+        self.__profile_image = profile_image
+        self.__role = "user"
+        
+class Admin(Account):
+    def __init__(self, id, email, password, login_status):
+        super().__init__(id, email, password, login_status)
+        self.__role = "admin"
+        
+
+class Review:
+    def __init__(self, rating, review_text, reviewer_name):
+        self._rating = rating
+        self._review_text = review_text
+        self._reviewer_name = reviewer_name
+
+class Landmark:
+    def __init__(self, name, description, latitude, longitude, address, image, type):
+        self._name = name
+        self._description = description
+        self._latitude = latitude
+        self._longtitude = longitude
+        self._address = address
+        self._image = image
+        self._type = type
+    
+
+# มี class ที่มี arttibute เหมือนกัน
+class Search:
+    def __init__(self):
+        pass
+
+class LandmarkCatalog:
+    def __init__(self, landmarks):
+        self._landmarks = [] # list of landmark object
+
+class FavoriteCatalog:
+    def __init__(self, landmarks):
+        self._landmarks = [] # list of landmark object
+
+class Waypoint(Landmark):
+    def __init__(self, order, name, description, latitude, longitude, address, image, type):
+        super().__init__(name, description, latitude, longitude, address, image, type)
+        self._order = order
+
+class Roadtrip:
+    def __init__(self, title, start, end, waypoint, total_waypoint, distance):
+        self._title = title
+        self._start = start
+        self._end = end
+        self._waypoint = waypoint
+        self._total_waypoint = total_waypoint
+        self._distance = distance
+
+class RoadtripCatalog:
+    def __init__(self, roadtrips):
+        self._roadtrips = [] # list of roadtrip objects
+
+class RoadtripGuide:
+    def __init__(self, subtitle, introduction_text, ending_text, waypoints):
+        self._subtitle = subtitle
+        self._introduction_text = introduction_text
+        self._ending_text = ending_text
+        self._waypoints = [] # list of waypoints in that roadtrip
+
+class WaypointDetail:
+    def __init__(self, waypoint_id, text):
+        self._waypoint_id = waypoint_id
+        self._text = text
+
+class LandmarkType(Enum):
+    outdoor, restaurants, camp, activity, shopping, transportation_service, sport_and_wellness, fuel_and_rest_stop\
+    = 1, 2, 3, 4, 5, 6, 7, 8
+    
+class Role(Enum):
+    user,admin\
+    = 0, 1
