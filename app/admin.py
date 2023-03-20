6155f0066e23187cc3692578926dd2925ed8c1d7
Norawit Kositpuntawong <norawit512@gmail.com>
2023-03-20 21:36:07 +0700

    feat: add class Admin's method

--- app/admin.py
+++ app/admin.py
@@ -4,3 +4,12 @@ class Admin(Account):
     def __init__(self, id, email, password, login_status):
         super().__init__(id, email, password, login_status)
         self.__role = "admin"
+    
+    def edit_magazine(self, Magazine, section):
+        pass
+
+    def create_magaine(self, name, Roadtrip):
+        pass
+    
+    def remove_user(self, user):
+        pass
