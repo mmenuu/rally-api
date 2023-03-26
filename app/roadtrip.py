ce4c1c94eb2059bc18ba7ee5565d004ce96ad214
Norawit Kositpuntawong <norawit512@gmail.com>
2023-03-26 15:23:05 +0700

    feat: update class

--- app/roadtrip.py
+++ app/roadtrip.py
@@ -1,20 +1,10 @@
 class Roadtrip:
-    def __init__(self, title, start, end, waypoint, total_waypoint, distance):
-        self._title = title
-        self._start = start
-        self._end = end
-        self._waypoint = waypoint
-        self._total_waypoint = total_waypoint
-        self._distance = distance
-    
-    def add_waypoint(self, landmark):
-        pass
+    def __init__(self, title, waypoints, author, category):
+        self.title = title
+        self.waypoints = []
+        self.author = author
+        self.category = category
 
-    def remove_waypoint(self, waypoint):
-        pass
+    def add_waypoint(self, new_waypoint):
+        self.waypoints.append(new_waypoint)
 
-    def change_waypoint_order(self, waypoint01, waypoint02):
-        pass
-    
-    def edit_waypoint_detail(self, waypoint, text):
-        pass
--- app/roadtripCatalog.py
+++ app/roadtripCatalog.py
@@ -1,6 +1,17 @@
+import re
 class RoadtripCatalog:
-    def __init__(self, roadtrips):
-        self._roadtrips = [] # list of roadtrip objects
-    
-    def search_roadtrip(self, search_str):
-        pass
+    def __init__(self, roadtrips = None | list):
+        self.roadtrips = []
+
+    def add_roadtrip(self, roadtrip):
+        self.roadtrips.append(roadtrip)
+
+    def search(self, keyword):
+        regex = re.compile(keyword, re.IGNORECASE)
+        search_result = [item for item in self.roadtrips \
+            if regex.search(item.title) or \
+                regex.search(item.author) or \
+                regex.search(item.category) or \
+                [waypoint for waypoint in item.waypoints if regex.search(waypoint.title)]]
+
+        return search_result
