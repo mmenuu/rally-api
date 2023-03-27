101d37b175d05abb1324126d9b054ad13954781e
mmenuu <rctkhtp@gmail.com>
2023-03-28 02:22:14 +0700

    feat: favorite landmark and catalog

--- app/favorite_catalog.py
+++ app/favorite_catalog.py
@@ -1,12 +1,32 @@
-class FavoriteCatalog:
-    def __init__(self, landmark):
-        self.landmarks = [] # list of landmark object
-    
-    def add_favorite_landmark(self, list):
-        self.landmarks.append(list)
+from favorite_landmark import FavoriteLandmark
 
-    def remove_favorite_landmark(self, landmark):
-        pass
+class FavoriteCatalog:
+    def __init__(self):
+        self.favorite_landmarks = []
     
-    def get_favoritelandmark(self):
-        pass
+    def add_favorite_landmark(self, favorite_landmark):
+        self.landmarks.append(favorite_landmark)
+
+    def get_favorite_landmark_by_user_id(self, user_id: str):
+        for favorite_landmark in self.favorite_landmarks:
+            if favorite_landmark.user_id == user_id:
+                return favorite_landmark
+            
+        return None
+
+    def add_landmark_by_user_id(self, new_landmark, user_id: str):
+        favorite_landmark = self.get_favorite_landmark_by_user_id(user_id)
+        
+        if favorite_landmark is not None:
+            favorite_landmark.add_landmark(new_landmark)
+        else:
+            new_favorite_landmark = FavoriteLandmark(user_id)
+            new_favorite_landmark.add_landmark(new_landmark)
+            self.favorite_landmarks.append(new_favorite_landmark)            
+        
+
+    def remove_landmark_by_user_id(self, user_id: str, landmark_id:str):
+        favorite_landmark = self.get_favorite_landmark_by_user_id(user_id)
+        
+        if favorite_landmark is not None:
+            favorite_landmark.remove_landmark_by_landmark_id(landmark_id)
--- app/favorite_landmark.py
+++ app/favorite_landmark.py
@@ -1,13 +1,25 @@
 class FavoriteLandmark:
-    def __init__(self, user_id, landmarks = None | list):
-        self.landmarks_list = landmarks # list of landmark object
+    def __init__(self, user_id):
         self.user_id = user_id
-    
-    def add_favorite_landmark(self, landmark):
+        self.landmarks = [] # list of landmark object
+        
+    def add_landmark(self, landmark):
         self.landmarks_list.append(landmark)
+        
+    def get_landmarks(self):
+        return self.landmarks
 
-    def remove_favorite_landmark(self, landmark):
-        self.landmarks_list.remove(landmark)
-    
-    def get_favorite_landmark(self,user_id:str):
-        return [landmark for landmark in self.landmarks_list if self.user_id == user_id]
+    def get_landmark_by_landmark_id(self, landmark_id: str):
+        for landmark in self.landmarks:
+            if landmark.id == landmark_id:
+                return landmark
+
+        return None
+        
+    def remove_landmark_by_landmark_id(self, landmark_id: str):
+        landmark = self.get_landmark_by_landmark_id(landmark_id)
+        
+        if landmark is not None:
+            self.landmarks.remove(landmark)
+        
+        return False
