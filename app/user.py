0829ca3dc6e688ff23ec1fcd3ff7bd070325112a
1tpp <panapat.dev@gmail.com>
2023-03-29 14:01:46 +0700

    feat: user role

--- app/user.py
+++ app/user.py
@@ -2,4 +2,5 @@ from account import Account
 
 class User(Account):
     def __init__(self, email, username, password):
-        super().__init__(email, username, password)
+        super().__init__(id, email, username, password)
+        self.__role = "user"
