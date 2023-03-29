136264db9c0b1760e97985a00c232427200d4ae8
Norawit Kositpuntawong <norawit512@gmail.com>
2023-03-29 15:00:23 +0700

    refactor: magazine

--- app/magazine.py
+++ app/magazine.py
@@ -1,20 +1,27 @@
 from roadtrip import Roadtrip
+import uuid
 class Magazine:
     def __init__(self, id: str, roadtrip: Roadtrip, name, description):
-        self._id = id
-        self._roadtrip = roadtrip
-        self._name = name
-        self._description = description
+        self.__id = id
+        self.__roadtrip = roadtrip
+        self.__name = name
+        self.__description = description
 
     def set_magazine_roadtrip(self, roadtrip: Roadtrip):
-        self._roadtrip = roadtrip
+        self.__roadtrip = roadtrip
     
     def set_magazine_name(self, name: str):
-        self._name = name
+        self.__name = name
         
     def set_magazine_description(self, text: str):
-        self._description = text
+        self.__description = text
+
+    def get_magazine_roadtrip(self):
+        return self.__roadtrip
+    
+    def get_magazine_name(self):
+        return self.__name
 
     def get_magazine_id(self):
-        return self._id
+        return self.__id
 
