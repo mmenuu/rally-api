220059b0a8434eb110876208ce4678f2d72b13c4
1tpp <panapat.dev@gmail.com>
2023-04-02 16:14:15 +0700

    chore: add magazine route

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 
-from .routers import users, auth, roadtrips
+from .routers import users, auth, roadtrips, magazines
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(magazines.router)
 app.include_router(roadtrips.router)
 
 @app.get("/")
