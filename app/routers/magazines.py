6d7a3e6b907a59888e6ccfd6e0243d0cc09b0f80
Norawit Kositpuntawong <norawit512@gmail.com>
2023-04-08 15:04:35 +0700

    chore: refactor magazines

--- app/routers/magazines.py
+++ app/routers/magazines.py
@@ -3,6 +3,7 @@ from typing import Annotated
 
 from ..dependencies import get_current_user
 from ..databases import magazines_collection, roadtrips_collection
+
 from ..internal.magazine import Magazine
 from ..internal.user import User
 
@@ -17,12 +18,30 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-async def read_magazines():
+@router.get("/", status_code=status.HTTP_200_OK)
+async def get_all_magazine():
     '''
-    # get all magazine
+    # get all magazine objects in magazine catalog
     '''
-    return magazines_collection.get_magazines()
+    all_magazines = magazines_collection.get_magazines()
+
+    if all_magazines is None:
+        raise HTTPException(status_code=404, detail="No magazines found")    
+    
+    return all_magazines
+
+
+@router.get("/{magazine_id}", status_code=status.HTTP_200_OK)
+async def get_magazine_by_id(magazine_id: str):
+    '''
+    # get magazine by id
+    '''
+    magazine_exists = magazines_collection.get_magazine_by_id(magazine_id)
+
+    if magazine_exists is None:
+        raise HTTPException(status_code=404, detail="No magazines found")
+
+    return magazine_exists
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
@@ -48,36 +67,13 @@ async def create_magazine(body: dict):
         "detail": "magazine created successfully",
     }
 
-@router.get("/")
-async def get_all_magazine():
-    '''
-    # get all magazine objects in magazine catalog
-    '''
-    all_magazines = magazines_collection.get_magazines()
-
-    if not all_magazines:
-        raise HTTPException(status_code=404, detail="No magazines found")    
-    
-    return all_magazines
-
-
-@router.get("/{magazine_id}", status_code=status.HTTP_200_OK)
-async def get_magazine_by_id(magazine_id: str):
-    '''
-    # get magazine by id
-    '''
-    magazine_exists = magazines_collection.get_magazine_by_id(magazine_id)
-
-    if not magazine_exists:
-        raise HTTPException(status_code=404, detail="No magazines found")
-
-    return magazine_exists
-
 
 @router.patch("/{magazine_id}", status_code=status.HTTP_200_OK)
 async def update_magazine(magazine_id: str, body: dict):
     '''
     #edit magazine by id
+    @param magazine_id: `str` id of magazine
+
     ### request body
      - magazine_id: `str` required
      - title: `str` optional
@@ -91,7 +87,7 @@ async def update_magazine(magazine_id: str, body: dict):
     if magazine_exists is None:
         raise HTTPException(status_code=404, detail="Magazine not found")
 
-    magazine_exists.set_name(body.get("title", magazine_exists.get_title()))
+    magazine_exists.set_title(body.get("title", magazine_exists.get_title()))
     magazine_exists.set_description(body.get("description", magazine_exists.get_description()))
 
     return {
@@ -99,13 +95,18 @@ async def update_magazine(magazine_id: str, body: dict):
     }
 
 
-@router.delete("/{magazine_id}")
+@router.delete("/{magazine_id}", status_code=status.HTTP_200_OK)
 async def delete_roadtrip(magazine_id: str):
     '''
     # delete magazine by id
+    @param roadtrip_id: `str` id of the roadtrip
     '''
     magazine_exists = magazines_collection.get_magazine_by_id(magazine_id)
+    if magazine_exists is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Magazine not found")
+    
     magazines_collection.remove_magazine(magazine_exists)
+    
     
     return {
         "detail": "magazine deleted successfully",
