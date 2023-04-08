93a2d9236a7cfc3e0ad201215e9da0bce3093ad2
1tpp <panapat.dev@gmail.com>
2023-04-09 06:12:06 +0700

    chore: change response token in body to headers

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -3,6 +3,7 @@ from typing import Annotated
 
 from fastapi import APIRouter, HTTPException, status, Depends
 from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.responses import JSONResponse
 
 from pydantic import BaseModel
 
@@ -24,12 +25,7 @@ router = APIRouter(
 settings = get_settings()
 
 
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-@router.post("/register", response_model=Token, status_code=status.HTTP_201_CREATED)
+@router.post("/register", status_code=status.HTTP_201_CREATED)
 async def register(body: dict):
     '''
     # Register a new user
@@ -65,16 +61,23 @@ async def register(body: dict):
     # add user to collection
     users_collection.add_user(new_user)
 
-    return {
-        "detail": "User created successfully",
-        "access_token": create_access_token(
-            data={"sub": new_user.get_username()}
-        ),
-        "token_type": "bearer"
-    }
+    access_token_expires = timedelta(
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": new_user.get_username()}, expires_delta=access_token_expires
+    )
+
+    return JSONResponse(
+        status_code=201,
+        content={
+            "message": "User created successfully"
+        },
+        headers={
+            'Authorization': access_token
+        })
 
 
-@router.post("/login",  response_model=Token, status_code=status.HTTP_200_OK)
+@ router.post("/login", status_code=status.HTTP_200_OK)
 async def login(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
 ):
@@ -102,4 +105,11 @@ async def login(
     access_token = create_access_token(
         data={"sub": user.get_username()}, expires_delta=access_token_expires
     )
-    return {"access_token": access_token, "token_type": "bearer"}
+    return JSONResponse(
+        status_code=200,
+        content={
+            "message": "User logged in successfully"
+        },
+        headers={
+            "Authorization": access_token
+        })
